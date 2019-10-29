f462d2e46e43118102c04580f9a7172fd213ea5d
lstanleyCTL <william.stanley1@centurylink.com>
2019-10-29 18:36:43 +0000

    Lab 3

--- lab02.part2.main.js
+++ lab02.part2.main.js
@@ -6,23 +6,6 @@
 */
 const IPCIDR = require('ip-cidr');
 
-/*
-  Import the built-in path module.
-  See https://nodejs.org/api/path.html
-  The path module provides utilities for working with file and directory paths.
-  IAP requires the path module to access local file modules.
-  The path module exports an object.
-  Assign the imported object to variable path.
-*/
-const path = require('path');
-
-/**
- * Import helper function module located in the same directory
- * as this module. IAP requires the path object's join method
- * to unequivocally locate the file module.
- */
-const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
-
 /**
  * Calculates an IPv4-mapped IPv6 address.
  * @param {string} ipv4 - An IPv4 address in dotted-quad format.
